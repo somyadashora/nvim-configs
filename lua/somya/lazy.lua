1f2a3a322612afc24a8730a6f78f043dcd688408
Somya Dashora <38585070+somyadashora@users.noreply.github.com>
2024-09-09 17:32:03 +0000

    Add: till 51:00

--- lua/somya/lazy.lua
+++ lua/somya/lazy.lua
@@ -11,4 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("somya.plugins")
+require("lazy").setup("somya.plugins", {
+  change_detection = {
+    notify = false,
+  },
+})
