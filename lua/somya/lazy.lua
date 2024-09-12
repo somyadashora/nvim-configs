a9e287a1d8308e1d6f0ee718230d2e7d2c45d749
Somya Dashora <38585070+somyadashora@users.noreply.github.com>
2024-09-12 16:26:32 +0000

    finished basic nvim setup

--- lua/somya/lazy.lua
+++ lua/somya/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("somya.plugins", {
+require("lazy").setup({ { import = "somya.plugins" }, {import = "somya.plugins.lsp"} }, {
   change_detection = {
     notify = false,
   },
