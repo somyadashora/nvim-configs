4a2874856ace5559ddf7f2a9904424efd7995acb
Somya Dashora <38585070+somyadashora@users.noreply.github.com>
2024-10-22 18:00:05 +0530

    Update bufferline.lua

--- lua/somya/plugins/bufferline.lua
+++ lua/somya/plugins/bufferline.lua
@@ -9,6 +9,26 @@ return {
     },
   },
   config = function()
-    require("bufferline").setup{}
+    require("bufferline").setup{
+      options = {
+        buffer_close_icon = "x",
+        close_icon = "x",
+        always_show_bufferline = true,
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = "Fle Explorer",
+            text_align = "left",
+            seperator = true
+          }
+        }
+      }
+    }
+    
+    local keymap = vim.keymap
+
+    keymap.set("n", "<leader>bg", "<cmd>BufferLinePick<cr>", {desc = "Naviation to open buffer using mapped keys"})
+    keymap.set("n", "<leader>bn", "<cmd>BufferLineCycleNext<cr>", {desc = "Naviation to Next buffer in bufferlist"})
+    keymap.set("n", "<leader>bp", "<cmd>BufferLineCyclePrev<cr>", {desc = "Naviation to prev buffer in bufferlist"})
   end
 }
