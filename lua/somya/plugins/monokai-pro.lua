bf5a958131bcb7d30c766294d85fb89d62930ed8
Somya Dashora <38585070+somyadashora@users.noreply.github.com>
2024-11-13 18:16:09 +0530

    Update monokai-pro.lua

--- lua/somya/plugins/monokai-pro.lua
+++ lua/somya/plugins/monokai-pro.lua
@@ -1,32 +1,32 @@
 return {
-	-- Monokai Pro theme with multiple filters: Pro, Classic, Machine, Octagon,
-	-- Ristretto, Spectrum.
-	{
-		'loctvl842/monokai-pro.nvim',
-		lazy = false,
-		priority = 1000,
-		opts = {
-			filter = 'pro', -- classic | octagon | pro | machine | ristretto | spectrum
-			styles = {
-                        comment = { italic = true },
-                        keyword = { italic = true }, -- any other keyword
-                        type = { italic = true }, -- (preferred) int, long, char, etc
-                        storageclass = { italic = true }, -- static, register, volatile, etc
-                        structure = { italic = true }, -- struct, union, enum, etc
-                        parameter = { italic = true }, -- parameter pass in function
-                        annotation = { italic = true },
-                        tag_attribute = { italic = true }, -- attribute of tag in reactjs
-                        },
-			plugins = {
-				bufferline = {
-					underline_selected = false,
-					underline_visible = false,
-				},
-				indent_blankline = {
-					context_highlight = 'pro', -- default | pro
-					context_start_underline = false,
-				},
-			},
-		},
-	},
+-- Monokai Pro theme with multiple filters: Pro, Classic, Machine, Octagon,
+-- Ristretto, Spectrum.
+{
+  'loctvl842/monokai-pro.nvim',
+   lazy = false,
+   priority = 1000,
+   opts = {
+     filter = 'pro', -- classic | octagon | pro | machine | ristretto | spectrum
+     styles = {
+       comment = { italic = true },
+       keyword = { italic = true }, -- any other keyword
+       type = { italic = true }, -- (preferred) int, long, char, etc
+       storageclass = { italic = true }, -- static, register, volatile, etc
+       structure = { italic = true }, -- struct, union, enum, etc
+       parameter = { italic = true }, -- parameter pass in function
+       annotation = { italic = true },
+       tag_attribute = { italic = true }, -- attribute of tag in reactjs
+     },
+     plugins = {
+       bufferline = {
+         underline_selected = false,
+         underline_visible = false,
+       },
+       indent_blankline = {
+         context_highlight = 'pro', -- default | pro
+         context_start_underline = false,
+       },
+     },
+   },
+},
 }
