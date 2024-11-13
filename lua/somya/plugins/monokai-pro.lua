951719618ef6338190485274708f8708cd03545c
Somya Dashora <38585070+somyadashora@users.noreply.github.com>
2024-11-13 18:48:33 +0530

    Update monokai-pro.lua

--- lua/somya/plugins/monokai-pro.lua
+++ lua/somya/plugins/monokai-pro.lua
@@ -9,13 +9,13 @@ return {
      filter = 'pro', -- classic | octagon | pro | machine | ristretto | spectrum
      styles = {
        comment = { italic = true },
-       keyword = { italic = true }, -- any other keyword
-       type = { italic = true }, -- (preferred) int, long, char, etc
-       storageclass = { italic = true }, -- static, register, volatile, etc
-       structure = { italic = true }, -- struct, union, enum, etc
-       parameter = { italic = true }, -- parameter pass in function
-       annotation = { italic = true },
-       tag_attribute = { italic = true }, -- attribute of tag in reactjs
+       keyword = { italic = false }, -- any other keyword
+       type = { italic = false }, -- (preferred) int, long, char, etc
+       storageclass = { italic = false }, -- static, register, volatile, etc
+       structure = { italic = false }, -- struct, union, enum, etc
+       parameter = { italic = false }, -- parameter pass in function
+       annotation = { italic = false },
+       tag_attribute = { italic = false }, -- attribute of tag in reactjs
      },
      plugins = {
        bufferline = {
