aa3fc16b2ff91cf39f4304c08c45d7ac6610297b
Somya Dashora <38585070+somyadashora@users.noreply.github.com>
2024-11-18 01:38:49 +0530

    Update telescope.lua

--- lua/somya/plugins/telescope.lua
+++ lua/somya/plugins/telescope.lua
@@ -35,7 +35,7 @@ return {
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fSi", "<cmd>Telescope live_grep no_ignore=true hidden=true<cr>", { desc = "Find string in cwd, don't respect gitignore" })
+    -- keymap.set("n", "<leader>fSi", "<cmd>Telescope live_grep no_ignore=true hidden=true<cr>", { desc = "Find string in cwd, don't respect gitignore" })
 
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
 
