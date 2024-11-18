5c3b81626466eca070337266328ebb25576d299f
Somya Dashora <38585070+somyadashora@users.noreply.github.com>
2024-11-19 00:45:44 +0530

    Update align.lua

--- lua/somya/plugins/align.lua
+++ lua/somya/plugins/align.lua
@@ -9,11 +9,10 @@ return {
         -- Create your mappings here
         keymap.set("x", "<leader>Ac", function()require'align'.align_to_char({ preview = true, length = 1 }) end, NS)
         keymap.set("x", "<leader>A2c", function()require'align'.align_to_char({ preview = true, length = 2 }) end, NS)
-    
+
         keymap.set("x", "<leader>As", function()require'align'.align_to_string({ preview = true, regex = false }) end, NS) -- Aligns to a string with previews
         keymap.set("x", "<leader>Ar", function()require'align'.align_to_string({ preview = true, regex = true }) end, NS) -- Aligns to a vim regex with previews
-    
+
         keymap.set("n", "<leader>Ap", function() local a = require'align' a.operator( a.align_to_string, { regex = false, preview = true, }) end, NS) --to align a paragraph to a string with previews
-    
     end
 }
