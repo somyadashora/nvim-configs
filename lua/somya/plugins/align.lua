5a53a183998aef0038f81908b48c57447019c740
Somya Dashora <38585070+somyadashora@users.noreply.github.com>
2024-11-19 00:36:31 +0530

    Update align.lua

--- lua/somya/plugins/align.lua
+++ lua/somya/plugins/align.lua
@@ -3,10 +3,17 @@ return {
     branch = "v2",
     lazy = true,
     init = function()
-    local NS = { noremap = true, silent = true }
+        local NS = { noremap = true, silent = true }
+        local keymap = vim.keymap -- for conciseness
 
-    keymap.set('x', 'ad', function() require'align'.align_to_char({ preview = true, length = 2 }) end, NS, {desc = "align by character with live preview"})
-    
         -- Create your mappings here
+        keymap.set("x", "<leader>Ac", function()require'align'.align_to_char({ preview = true, length = 1 }) end, NS)
+        keymap.set("x", "<leader>A2c", function()require'align'.align_to_char({ preview = true, length = 2 }) end, NS)
+    
+        keymap.set("x", "<leader>As", function()require'align'.align_to_string({ preview = true, regex = false }) end, NS) -- Aligns to a string with previews
+        keymap.set("x", "<leader>Ar", function()require'align'.align_to_string({ preview = true, regex = true }) end, NS) -- Aligns to a vim regex with previews
+    
+        keymap.set("n", "<leader>Ap", function() local a = require'align' a.operator( a.align_to_string, { regex = false, preview = true, }) end, NS) --to align a paragraph to a string with previews
+    
     end
 }
