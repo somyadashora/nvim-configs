c68d9b34c3dc0c2fdffa370384cc046e816e76c6
Somya Dashora <38585070+somyadashora@users.noreply.github.com>
2025-02-06 13:14:48 +0530

    Update todo-comments.lua with new keywords

--- lua/somya/plugins/todo-comments.lua
+++ lua/somya/plugins/todo-comments.lua
@@ -17,23 +17,39 @@ return {
     end, { desc = "Previous todo comment" })
 
     todo_comments.setup{
-      -- keywords recognized as todo comments
+      -- keywords recognized as todo comments in VLSI
       keywords = {
-        FIX = {
+        FIXME = {
           -- icon = "? ", -- icon used for the sign, and in search results
           color = "error", -- can be a hex color, or a named color (see below)
-          alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+          alt = { "FIX" }, -- a set of other keywords that all map to this FIX keywords
           -- signs = false, -- configure signs for some keywords individually
         },
         TODO = { color = "info" },
-        HACK = { color = "warning" },
-        WARN = { color = "warning", alt = { "WARNING", "XXX", "REVIEW" } },
-        PERF = { alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE", "IMPORTANT", "LGTM" } },
+        REVIEW = { color = "warning", alt = { "WARNING" } },
+        PERF = { alt = { "IMPORTANT" } },
         SPEC = { alt = { "SPECIFICATION" } },
         NOTE = { color = "hint", alt = { "INFO" } },
-        TEST = { color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
       },
 
+      -- keywords recognized as todo comments
+      -- keywords = {
+      --   FIX = {
+      --     -- icon = "? ", -- icon used for the sign, and in search results
+      --     color = "error", -- can be a hex color, or a named color (see below)
+      --     alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+      --     -- signs = false, -- configure signs for some keywords individually
+      --   },
+      --   TODO = { color = "info" },
+      --   HACK = { color = "warning" },
+      --   WARN = { color = "warning", alt = { "WARNING", "XXX", "REVIEW" } },
+      --   PERF = { alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE", "IMPORTANT", "LGTM" } },
+      --   SPEC = { alt = { "SPECIFICATION" } },
+      --   NOTE = { color = "hint", alt = { "INFO" } },
+      --   TEST = { color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+      -- },
+
+      
 
     }
   end,
